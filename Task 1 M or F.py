5b91feb2aa2581b381af0b005d46898f300a25c2
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:01:12 +0530

    My next task(1)(6)

--- Task 1 M or F.py
+++ Task 1 M or F.py
@@ -1,7 +1,13 @@
+#TASK 1
+#6)find user is Male or Female
+#input:Male
+#ouput:user is Male
+
+
 Gender = input("Enter your gender: ")
-if Gender == "Male":
+if Gender == "Male" or Gender == "male" or Gender == "MALE":
     print("User is Male")
-elif Gender == "Female":
+elif Gender == "Female" or Gender == "female" or Gender == "FEMALE":
     print("User is Female")
 else:
     print("Invalid input")
